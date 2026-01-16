ce792eb278a125b7344cdf6637145ed012db440c
Michu-s <brandon121005@gmail.com>
2026-01-16 12:22:34 -0800

    correccion direccion supabase

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,7 @@ void main() async {
   const supabaseUrl = 'https://wtlitcaiboefcujqrmrg.supabase.co';
   const supabaseAnonKey = 'sb_publishable_ZE7_csZsdp62HMNIocfbWQ_ogId0bVq';
 
-  await Supabase.initialize(
-    url: 'https://wtlitcaiboefcujqrmrg.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0bGl0Y2FpYm9lZmN1anFybXJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzMjY1MjksImV4cCI6MjA3OTkwMjUyOX0.uNEVVt9HCCBTnKhdv3hNHDKGrb2rTJAp2wJIA24_EgE',
-  );
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
 
   if (kDebugMode) {
     debugPrint('Supabase URL configurada: $supabaseUrl');

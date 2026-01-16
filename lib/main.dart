f4ff870c3d45b5b9f16449220e4e21593e72ce29
MartinC <cabreramartin362@gmail.com>
2026-01-16 12:26:46 -0600

    correccion de widget_test

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   const supabaseUrl = 'https://wtlitcaiboefcujqrmrg.supabase.co';
-  const supabaseAnonKey = 'sb_publishable_ZE7_csZsdp62HMNIocfbWQ_ogId0bVq';
+  // const supabaseAnonKey = '...'; // Removed unused key
 
   await Supabase.initialize(
-    url: 'https://wtlitcaiboefcujqrmrg.supabase.co',
+    url: supabaseUrl,
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0bGl0Y2FpYm9lZmN1anFybXJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzMjY1MjksImV4cCI6MjA3OTkwMjUyOX0.uNEVVt9HCCBTnKhdv3hNHDKGrb2rTJAp2wJIA24_EgE',
   );
